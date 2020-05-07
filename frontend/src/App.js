6b31f5e881164f04c508685123834eb0685f147c
Mian Uddin <mianuddin@gmail.com>
2020-05-07 10:47:57 -0700

    remove old code from app (closes #39)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import Parse from 'parse';
 
-import Review from "./Review";
 import AddItem from "./AddItem";
 import ArtistList from "./ArtistList";
 import SubmitReview from "./SubmitReview";
@@ -13,69 +12,8 @@ function App() {
   Parse.initialize("HjKymbNGAhUhWwGSAmMDevlJJzVQPgworMQ9Fbud", "");
   Parse.serverURL = "http://localhost:1337/parse";
 
-  const [items, setItems] = useState([]);
-  const fetchItems = useCallback(() => {
-    const fetchData = async () => {
-      const query = new Parse.Query(Parse.Object.extend("review"));
-      const result = await query.find();
-
-      setItems(result);
-    };
-
-    fetchData();
-  }, []);
-
-  const [targetData, setTargetData] = useState({});
-  const fetchItem = useCallback((id) => {
-    const fetchData = async () => {
-      const query = new Parse.Query(Parse.Object.extend("review"));
-      const result = await query.get(id);
-
-      setTargetData(result.toJSON());
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div
-      style={{width: "80vw", margin: "0 auto"}}
-    >
-      <div>
-        <h2>Items in Database</h2>
-        <button type="button" onClick={fetchItems}>Fetch Items</button>
-        {items.map(item => (
-          <div key={item.id}>
-            <p>{item.id}</p>
-            <ul>
-              {Object.entries(item).map(([key, value]) => (
-                <li key={`${item.id}-${key}`}>{key}: {value}</li>
-              ))}
-            </ul>
-          </div> 
-        ))}
-      </div>
-      <div>
-        <h2>View Item from Database</h2>
-        <div>
-          <label htmlFor="oId">
-            Object Id:
-            <input
-              type="text"
-              id="oId"
-              name="oId"
-              onKeyPress={event => (event.key === "Enter") && fetchItem(event.target.value)}
-            />
-          </label>
-        </div>
-        <Review
-          artist={targetData.artist}
-          song={targetData.song}
-          userId={targetData.userId}
-          rating={targetData.rating}
-          review={targetData.review}
-        />
-      </div>
+    <div style={{width: "80vw", margin: "0 auto"}}>
       <AddItem />
       <ArtistList />
       <SubmitReview userId={1} />
