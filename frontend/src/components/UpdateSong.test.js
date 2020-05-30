e10a1163c8cfe308b27bb6077487626bcb2afde8
Mian Uddin <mianuddin@gmail.com>
2020-05-30 10:34:47 -0700

    test that the song can be updated successfully

--- frontend/src/components/UpdateSong.test.js
+++ frontend/src/components/UpdateSong.test.js
@@ -100,6 +100,48 @@ it("shows an error if the URL is not valid", async () => {
   expect(queryByText(/Needs to be a valid link/i)).toBeTruthy();
 });
 
+it("shows a message on success", async () => {
+  // Arrange
+  const propSongId = "fakeSongId";
+  const propSongName = "fakeSongName";
+  const propSongArt = "https://localhost/cover.jpg";
+  const inputSongName = "fakeNewSongName";
+  const inputCoverArt = "https://localhost/cover.jpg";
+
+  // Act
+  const {
+    queryByText,
+    getByLabelText
+  } = render(
+    <UpdateSong
+      songId={propSongId}
+      songName={propSongName}
+      songArt={propSongArt}
+    />
+  );
+  const songNameInputElement = getByLabelText(/^Song Name/i);
+  const coverArtInputElement = getByLabelText(/^Cover Art/i);
+  // https://react-hook-form.com/faqs#TestingReactHookForm
+  await act(async () => {
+    await fireEvent.input(
+      songNameInputElement,
+      {target: {value: inputSongName}}
+    );
+    await fireEvent.input(
+      coverArtInputElement,
+      {target: {value: inputCoverArt}}
+    );
+    await fireEvent.click(queryByText(/Update/, {selector: "input"}));
+  });
+
+  // Assert
+  expect(queryByText(/Song details updated!/i)).toBeTruthy();
+  expect(mockSongSet.mock.calls.length).toBe(2);
+  expect(mockSongSet.mock.calls[0][1]).toBe(inputSongName);
+  expect(mockSongSet.mock.calls[1][1]).toBe(inputCoverArt);
+  expect(mockSongSave.mock.calls.length).toBe(1);
+});
+
 it("allows specifying no album art", async () => {
   // Arrange
   const propSongId = "fakeSongId";
