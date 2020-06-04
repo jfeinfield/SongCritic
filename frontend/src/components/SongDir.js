cea4cc55759d6d0402a495b77b4542b93de1715d
Mian Uddin <mianuddin@gmail.com>
2020-06-04 10:56:48 -0700

    add artists to song dir

--- frontend/src/components/SongDir.js
+++ frontend/src/components/SongDir.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import Parse from "parse";
 import {Link} from "react-router-dom";
-import {Song as SongClass} from "../parseClasses";
+import {Song as SongClass, User as UserClass} from "../parseClasses";
 
 const SongDir = () => {
   const [songs, setSongs] = useState([]);
@@ -12,11 +12,29 @@ const SongDir = () => {
     (async () => {
       try {
         const songQuery = new Parse.Query(SongClass);
-        const songList = await songQuery.addAscending("name").find();
-        setSongs(songList.map((song) => song.toJSON()));
+        const songListPointers = await songQuery.addAscending("name").find();
+        const songList = songListPointers.map(async (s) => {
+          const {
+            artist: {objectId: artistId},
+            name,
+            objectId
+          } = s.toJSON();
+
+          const userQuery = new Parse.Query(UserClass);
+          let artist = await userQuery.get(artistId);
+          artist = artist.toJSON();
+
+          return {
+            artist: artist.name,
+            name,
+            objectId
+          };
+        });
+
+        setSongs(await Promise.all(songList));
         setFetchingSongs(false);
       } catch (error) {
-        setErrorMsg(error);
+        setErrorMsg(`${error.code} ${error.message}`);
       }
     })();
   }, []);
@@ -25,38 +43,44 @@ const SongDir = () => {
     <div>
       <h2>Songs</h2>
       {errorMsg !== ""
-        ? <p className="text-danger">{errorMsg}</p>
-        : <>
-          {fetchingSongs
-            ? (
-              <div className="text-center">
-                <div className="spinner-border text-primary" role="status">
-                  <span className="sr-only">Loading...</span>
+        ? (
+          <p className="text-danger mt-3">
+            <strong>Error {errorMsg.split(" ")[0]}</strong><br />
+            {errorMsg.split(" ").slice(1).join(" ")}
+          </p>
+        ) : (
+          <>
+            {fetchingSongs
+              ? (
+                <div className="text-center">
+                  <div className="spinner-border text-primary" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                  <p>Fetching songs...</p>
                 </div>
-                <p>Fetching artists...</p>
-              </div>
-            )
-            : (
-              <>
-                <p>
-                  There are <span className="text-info">{songs.length}</span>
-                  {" songs available for review on Song Critic."}
-                </p>
-                <ul className="list-group">
-                  {songs.map((song) => (
-                    <Link
-                      key={song.objectId}
-                      className="list-group-item list-group-item-action"
-                      to={`/song/${song.objectId}`}
-                    >
-                      {song.name}
-                    </Link>
-                  ))}
-                </ul>
-              </>
-            )
-          }
-        </>
+              )
+              : (
+                <>
+                  <p>
+                    There are <span className="text-info">{songs.length}</span>
+                    {" songs available for review on Song Critic."}
+                  </p>
+                  <ul className="list-group">
+                    {songs.map((song) => (
+                      <Link
+                        key={song.objectId}
+                        className="list-group-item list-group-item-action"
+                        to={`/song/${song.objectId}`}
+                      >
+                        <strong>{song.name}</strong> by {song.artist}
+                      </Link>
+                    ))}
+                  </ul>
+                </>
+              )
+            }
+          </>
+        )
       }
     </div>
   );
