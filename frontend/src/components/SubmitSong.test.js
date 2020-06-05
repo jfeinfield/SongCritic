2e03addfa9551f09d0e49485787a74dc43d3895b
jande141 <43483803+jande141@users.noreply.github.com>
2020-06-04 21:09:36 -0700

    Update frontend/src/components/SubmitSong.test.js
    Co-authored-by: Mian Uddin <mianuddin@gmail.com>

--- frontend/src/components/SubmitSong.test.js
+++ frontend/src/components/SubmitSong.test.js
@@ -146,6 +146,6 @@ it("allows specifying no album art", async () => {
 
   // Assert
   expect(mockSongSet.mock.calls.length).toBe(3);
-  expect(mockSongSet.mock.calls[1][1]).toBe(inputCoverArt);
+  expect(mockSongSet.mock.calls[2][1]).toBe(inputCoverArt);
   expect(mockSongSave.mock.calls.length).toBe(1);
 });
